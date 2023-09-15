345e140889b892f3351e08db62c960d44e9de4b9
MRAlirad <m.r.alirad75@gmail.com>
2023-09-15 18:28:14 +0330

    coach item

---
+++ src/components/CoachItem.vue
@@ -0,0 +1,61 @@
+<template>
+	<section class="coach-item">
+		<h3>{{ fullName }}</h3>
+		<h4>$ {{ rate }}/hour</h4>
+		<div class="experties">
+			<span
+				v-for="area in areas"
+				:key="area"
+				>{{ area }}</span
+			>
+		</div>
+		<div class="actions">
+			<router-link :to="coachContactLink">Contact</router-link>
+			<router-link :to="coachDetailLink">View Details</router-link>
+		</div>
+	</section>
+</template>
+
+<script>
+	export default {
+		props: ['id', 'firstName', 'LastName', 'rate', 'areas'],
+		computed: {
+			fullName() {
+				return this.firstName + ' ' + this.LastName;
+			},
+			coachContactLink() {
+				return `${this.$route.path}/${this.id}/contact`;
+			},
+			coachDetailLink() {
+				return `${this.$route.path}/${this.id}`;
+			},
+		},
+	};
+</script>
+
+<style scoped>
+	.coach-item {
+		margin: 1rem 0;
+		border: 1px solid #424242;
+		border-radius: 12px;
+		padding: 1rem;
+	}
+
+	h3 {
+		font-size: 1.5rem;
+	}
+
+	h3,
+	h4 {
+		margin: 0.5rem 0;
+	}
+
+	div {
+		margin: 0.5rem 0;
+	}
+
+	.actions {
+		display: flex;
+		justify-content: flex-end;
+	}
+</style>
--- src/pages/CoachesList.vue
+++ src/pages/CoachesList.vue
@@ -1,22 +1,43 @@
 <template>
 	<section>Filter</section>
-	<div class="coaches-list" v-if="hasCoaches">
-		<li v-for="coach in filteredCoaches" :key="coach.id"> {{ coach.firstName }}</li>
+	<div
+		class="coaches-list"
+		v-if="hasCoaches"
+	>
+		<CoachItem
+			v-for="coach in filteredCoaches"
+			:key="coach.id"
+			:id="coach.id"
+			:firstName="coach.firstName"
+			:LastName="coach.LastName"
+			:rate="coach.hourlyRate"
+			:areas="coach.areas"
+		/>
 	</div>
-	<h3 v-else> No Coaches Found </h3>
+	<h3 v-else>No Coaches Found</h3>
 </template>
 
 <script>
-export default {
-	computed: {
-		filteredCoaches(){
-			return this.$store.getters['coaches/coaches'];
+	import CoachItem from '../components/CoachItem.vue';
+
+	export default {
+		components: {
+			CoachItem,
 		},
-		hasCoaches(){
-			return this.$store.getters['coaches/hasCoaches'];
-		}
-	}
-};
+		computed: {
+			filteredCoaches() {
+				return this.$store.getters['coaches/coaches'];
+			},
+			hasCoaches() {
+				return this.$store.getters['coaches/hasCoaches'];
+			},
+		},
+	};
 </script>
 
-<style></style>
+<style scoped>
+	.controls {
+		display: flex;
+		justify-content: space-between;
+	}
+</style>
