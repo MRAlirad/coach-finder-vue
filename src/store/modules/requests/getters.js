704f9c1fe0246a81fdbd3e6b56463ce44b53cc16
MRAlirad <m.r.alirad75@gmail.com>
2023-09-16 12:33:55 +0330

    filtering requests for the active coach

--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -1,10 +1,11 @@
 export default {
-    requests(state)
+    requests(state, _, _2, rootGetters)
     {
-        return state.requests;
+        const coachId = rootGetters.userId;
+        return state.requests.filter(req => req.coachId === coachId);
     },
-    hasRequests(state)
+    hasRequests(_, getters)
     {
-        return state.requests && state.requests.length > 0;
+        return getters.requests && getters.requests.length > 0;
     }
 };
